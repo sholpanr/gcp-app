f91c8053feb1ff49278462f9b0208bbf5b9f731c
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-04-06 18:56:02 -0500

    restructured

--- gke.tf
+++ gke.tf
@@ -1,18 +1,13 @@
-
-resource "google_service_account" "default" {
-  account_id   = var.sa_id
-  display_name = var.sa_dn
+provider "google" {
+  project = "utopian-planet-418522"
+  region  = "us-central1"
 }
 
-resource "google_container_cluster" "primary" {
-  name               = var.cluster_name
-  location           = var.cluster_location
-  initial_node_count = var.cluster_initial_node_number
-  node_config {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    disk_size_gb    = var.disk_size_gb
-
-  }
-
+module "gke-cluster" {
+  source = "git@github.com:sholpanr/tf-modules.git"
+  sa_id            = "gke-cluster-1234"
+  sa_dn            = "Service Account for cluster"
+  cluster_name     = "cluster-1234"
+  cluster_location = "us-central1-c"
+  project_id       = "utopian-planet-418522"
 }
--- values.tfvars
+++
@@ -1,5 +0,0 @@
-sa_id            = "gke-cluster-1234"
-sa_dn            = "Service Account for cluster"
-cluster_name     = "cluster-1234"
-cluster_location = "us-central1-c"
-project_id       = "utopian-planet-418522"
