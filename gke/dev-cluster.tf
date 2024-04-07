7a1e711f399379618823ff614eaca7ffb9f206fe
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-04-07 15:25:41 -0500

    changed

--- gke/dev-cluster.tf
+++ gke/dev-cluster.tf
@@ -3,10 +3,10 @@ provider "google" {
   region  = "us-central1"
 }
 
-module "gke-cluster" {
+module "dev-cluster" {
   source = "git@github.com:sholpanr/tf-modules.git//gke"
-  sa_id            = "gke-cluster-1234"
-  sa_dn            = "Service Account for cluster"
+  sa_id            = "dev-cluster-1234"
+  sa_dn            = "Service Account for Dev cluster"
   cluster_name     = "cluster-1234"
   cluster_location = "us-central1-c"
   project_id       = "utopian-planet-418522"
