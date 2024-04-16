417155ded9ecf665392c097ee03c66deb2ae6edc
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-04-16 14:57:17 -0500

    added qa cluster

--- gke/dev-cluster.tf
+++ gke/dev-cluster.tf
@@ -1,13 +1,8 @@
-provider "google" {
-  project = "utopian-planet-418522"
-  region  = "us-central1"
-}
-
 module "dev-cluster" {
   source = "git@github.com:sholpanr/tf-modules.git//gke"
-  sa_id            = "dev-cluster-1234"
+  sa_id            = "dev-cluster-1"
   sa_dn            = "Service Account for Dev cluster"
-  cluster_name     = "cluster-1234"
+  cluster_name     = "dev-cluster"
   cluster_location = "us-central1-c"
   project_id       = "utopian-planet-418522"
 }
---
+++ gke/qa-cluster.tf
@@ -0,0 +1,9 @@
+
+module "qa-cluster" {
+  source = "git@github.com:sholpanr/tf-modules.git//gke"
+  sa_id            = "qa-cluster-1"
+  sa_dn            = "Service Account for QA cluster"
+  cluster_name     = "qa-cluster"
+  cluster_location = "us-central1-c"
+  project_id       = "utopian-planet-418522"
+}
